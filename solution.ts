aa1686c1cbec6371b48c08c450d2c40a1517b47a
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2026-04-19 00:43:32 +0600

    Add fizzBuzz function and comment out console.log for reverseString

--- solution.ts
+++ solution.ts
@@ -9,4 +9,35 @@ function reverseString(s: string): string {
     return reversedText;
 }
 
-console.log(reverseString("hello"));
+// console.log(reverseString("hello"));
+
+
+// 2. FizzBuzz
+function fizzBuzz(n: number): string[] {
+
+    const result: string[] = [];
+
+    for (let i = 1; i <= n; i++) {
+
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        }
+
+        else if (i % 3 === 0) {
+            result.push("Fizz");
+        }
+
+        else if (i % 5 === 0) {
+            result.push("Buzz");
+        }
+
+        else {
+            result.push(String(i));
+        }
+    }
+
+    return result;
+}
+
+
+// console.log(fizzBuzz(15));
